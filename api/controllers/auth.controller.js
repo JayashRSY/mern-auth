3faa44bd058b03aecd59d17457027530f5d90c2e
Jayash Raj Singh Yadav <jayash.yadav@geminisolutions.com>
2024-01-20 12:54:20 +0530

    singin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     try {
@@ -11,6 +13,25 @@ export const signup = async (req, res, next) => {
             message: "User created successfully",
             user: newUser,
         })
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const validUser = await User.findOne({ email });
+        if (!validUser) next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) next(errorHandler(401, "Invalid password"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+        const { password: hashedPassword, ...userDetails } = validUser._doc;
+        res.cookie('accessToken', token, { httpOnly: true })
+            .status(200).json({
+                message: "User logged in successfully",
+                user: userDetails,
+            })
     } catch (error) {
         next(error);
     }
